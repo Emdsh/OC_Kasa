354a020cf69c2f865542203385a465a8ce5b2f5b
Emdsh <embitiousdev@gmail.com>
2022-05-11 00:59:01 +0200

    feat: add banner

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Header from './Header'
 import Footer from './Footer'
+import Banner from './Banner';
 
 function App() {
   return (
     <>
       <div className='app'>
         <Header />
+        <Banner />
       </div>
 
       <Footer />
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+function Banner() {
+  return (
+    //add extra className "main" or "about" to change background image
+    // remove <p> if about page
+    <div className="banner main">
+      <p className="banner__text">Chez vous, partout et ailleurs</p>
+    </div>
+  )
+}
+
+export default Banner
