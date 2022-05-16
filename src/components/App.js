ea536c8e410f76c6e5125f94f9ec8fac3354a0a3
Emdsh <embitiousdev@gmail.com>
2022-05-16 13:29:36 +0200

    feat: add 404 error page

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Header from './Header'
 import Footer from './Footer'
 import Banner from './Banner'
 import Portfolio from './Portfolio'
+import Error404 from './Error404'
 
 function App() {
   return (
@@ -10,6 +11,11 @@ function App() {
         <Header />
         <Banner />
         <Portfolio />
+
+        <div style={{height: '10px', background: '#000', margin: "20px 0"}}></div>
+
+        <Header />
+        <Error404 />
       </div>
 
       <Footer />
