231b46801fb83ef6b929a68f854f3d1ebe47b2ec
Emdsh <embitiousdev@gmail.com>
2022-05-16 14:53:09 +0200

    feat(portfolio): introduce props

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,12 @@
+import image from '../assets/images/about_banner_1440w.jpg'
 import PortfolioTile from './PortfolioTile'
 
 function Portfolio() {
+  const title = 'Titre de la location';
+
   const listItems = [];
   for (let i = 0; i < 7; i += 1) {
-    listItems.push(<li key={`li-${i}`}><PortfolioTile /></li>)
+    listItems.push(<li key={`li-${i}`}><PortfolioTile image={image} title={title} /></li>)
   }
 
   return (
--- src/components/PortfolioTile.js
+++ src/components/PortfolioTile.js
@@ -1,13 +1,14 @@
-import image from '../assets/images/about_banner_1440w.jpg'
+function PortfolioTile(props) {
 
-function PortfolioTile() {
+  const {image, title} = props;
+
   return (
     <a href="./titre-de-la-location">
       <article className="portfolio__tile">
         <figure className="portfolio__tile__content">
           <img src={image} alt="" className="portfolio__tile__image"/>
           <figcaption className="portfolio__tile__caption">
-            <h2 className="portfolio__tile__title">Titre de la location</h2>
+            <h2 className="portfolio__tile__title">{title}</h2>
           </figcaption>
         </figure>
       </article>
