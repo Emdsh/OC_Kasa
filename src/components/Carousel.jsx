b3532a52b4c1f6835fca1c96b6c2c788d76951cc
Emdsh <embitiousdev@gmail.com>
2022-05-23 19:53:12 +0200

    feat(Carousel): add navigation actions

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,15 +1,31 @@
-import image from '../assets/images/temp/temp_1440w.jpg'
+import { useState } from 'react'
 
-function Carrousel() {
+import image from '../assets/images/temp/temp_1440w.jpg';
+import image2 from '../assets/images/main_banner_1440w.jpg';
+import image3 from '../assets/images/about_banner_1440w.jpg';
+
+function Carousel() {
+  const carousel = [{image}, {image2}, {image}, {image3}]
+  const [imageIndex, navigationClick] = useState(0)
+
   return(
     <figure className='carousel'>
-      <img src={image} alt='' className='carousel__image'/>
+      <img src={carousel[imageIndex].image} alt='' className='carousel__image'/>
       <figcaption className='carousel__caption'>
-        <p>1/1</p>
+        <p>{imageIndex + 1}/{carousel.length}</p>
       </figcaption>
-      <span className='carousel__navigation'></span>
+      {carousel.length > 1 &&
+        <div className='carousel__navigation'>
+          <span className='carousel__navigation--backward' 
+            onClick={() => navigationClick((imageIndex - 1) < 0 ? (carousel.length - 1) : (imageIndex - 1))}>
+          </span>
+          <span className='carousel__navigation--forward'
+            onClick={() => navigationClick((imageIndex + 1) > (carousel.length - 1) ? 0 : (imageIndex + 1))}>
+          </span>
+        </div>
+      }
     </figure>
   )
 }
 
-export default Carrousel
+export default Carousel
