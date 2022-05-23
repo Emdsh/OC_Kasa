d6f2c4851e9b69d32ef337af15889f6adfe768da
Emdsh <embitiousdev@gmail.com>
2022-05-23 19:52:30 +0200

    refactor(Rating): simplify code

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,8 +2,8 @@ function Rating(props) {
   const {rate} = props
 
   const rating = []
-  for (let i = 0; i < 5; i += 1) {
-    rating.push(<span key={`star-${i+1}`} className={`star ${i+1 <= rate? 'full' : ''}`}></span>)
+  for (let i = 1; i < 6; i += 1) {
+    rating.push(<span key={`star-${i}`} className={`star ${i <= rate? 'full' : ''}`}></span>)
   }
 
 
