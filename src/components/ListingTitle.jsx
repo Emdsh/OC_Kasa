331b7960b03ff9f29ce9aa760391a1a2f788ad19
Emdsh <embitiousdev@gmail.com>
2022-05-24 19:44:38 +0200

    fix(style): better handling of unpredictable content

--- src/components/ListingTitle.jsx
+++ src/components/ListingTitle.jsx
@@ -2,8 +2,8 @@ function ListingTitle(props) {
   const {title, location} = props
 
   return (
-    <div className="listing-details__title">
-      <h1 className="listing-details__name">{title}</h1>
+    <div className="listing-details__text">
+      <h1 className="listing-details__title">{title}</h1>
       <p className="listing-details__location">{location}</p>
     </div>
   )
