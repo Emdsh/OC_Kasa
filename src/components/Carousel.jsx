6acabd8853caf572f5bd5790410f0c39827503a2
Emdsh <embitiousdev@gmail.com>
2022-05-24 17:52:00 +0200

    feat(accessibility): add keyboard navigation to carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import image from '../assets/images/temp/temp_1440w.jpg';
 import image2 from '../assets/images/main_banner_1440w.jpg';
@@ -8,6 +8,25 @@ function Carousel() {
   const carousel = [{image}, {image2}, {image}, {image3}]
   const [imageIndex, navigationClick] = useState(0)
 
+  function navigateBackward() {
+    navigationClick((imageIndex - 1) < 0 ? (carousel.length - 1) : (imageIndex - 1))
+  }
+
+  function navigateForward() {
+    navigationClick((imageIndex + 1) > (carousel.length - 1) ? 0 : (imageIndex + 1))
+  }
+
+  function manageKeyPress(event) {
+    if (event.key === 'ArrowLeft') {
+      navigateBackward()
+    }
+    if (event.key === 'ArrowRight') {
+      navigateForward()
+    }
+  }
+
+  useEffect(() => {document.addEventListener('keydown', manageKeyPress)})
+
   return(
     <figure className='carousel'>
       <img src={carousel[imageIndex].image} alt='' className='carousel__image'/>
@@ -16,12 +35,10 @@ function Carousel() {
       </figcaption>
       {carousel.length > 1 &&
         <div className='carousel__navigation'>
-          <span className='carousel__navigation--backward' 
-            onClick={() => navigationClick((imageIndex - 1) < 0 ? (carousel.length - 1) : (imageIndex - 1))}>
-          </span>
+          <span className='carousel__navigation--backward'
+            onClick={navigateBackward}></span>
           <span className='carousel__navigation--forward'
-            onClick={() => navigationClick((imageIndex + 1) > (carousel.length - 1) ? 0 : (imageIndex + 1))}>
-          </span>
+            onClick={navigateForward}></span>
         </div>
       }
     </figure>
