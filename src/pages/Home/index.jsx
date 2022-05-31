fc63c01dfda74e9754095428a84d3044b2faa155
Emdsh <embitiousdev@gmail.com>
2022-05-31 14:18:28 +0200

    feat(Loader): add loader to Home and Listing page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,23 @@
 import Banner from '../../components/Banner'
 import Portfolio from '../../layouts/Portfolio'
 
+import { useContext } from 'react'
+import { ListingsContext } from '../../API/Listings'
+import Loader from '../../components/Loader'
+
 function Home() {
+  const { isDataLoading: loader } = useContext(ListingsContext)
+  
   return (
     <>
-      <Banner page="home" title="Chez vous, partout et ailleurs" />
-      <Portfolio />
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <Banner page="home" title="Chez vous, partout et ailleurs" />
+          <Portfolio />
+        </>
+      )}
     </>
   )
 }
--- src/pages/Listing/index.jsx
+++ src/pages/Listing/index.jsx
@@ -3,15 +3,26 @@ import ListingDetails from '../../layouts/Listing/Details'
 
 import { useParams } from 'react-router-dom'
 
+import { useContext } from 'react'
+import { ListingsContext } from '../../API/Listings'
+import Loader from '../../components/Loader'
+
 function Listing() {
   let id = useParams()
+  const { isDataLoading: loader } = useContext(ListingsContext)
 
   return (
     <>
-      <Carousel id={id} />
-      <main>
-        <ListingDetails id={id} />
-      </main>
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <Carousel id={id} />
+          <main>
+            <ListingDetails id={id} />
+          </main>{' '}
+        </>
+      )}
     </>
   )
 }
